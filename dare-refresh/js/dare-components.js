cc515109c50efc6e75da60dd322dcbe35dd3b7c8
TheLastDinosaur <81168359+TheLastDinosaur@users.noreply.github.com>
2023-05-30 22:06:37 +0530

    DARE
    DARE

---
+++ dare-refresh/js/dare-components.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent('model-opacity', {
+    schema: {default: 1.0},
+    init: function () {
+        // console.log("Loaded");
+    //   this.el.addEventListener('model-loaded', this.update.bind(this));
+    },
+    update: function () {
+        // console.log("UPDATE EVE");
+      var mesh = this.el.getObject3D('mesh');
+      var data = this.data;
+      if (!mesh) { return; }
+      mesh.traverse(function (node) {
+        // console.log("THIS IS A MESH");
+        if (node.isMesh) {
+          node.material.opacity = data;
+          node.material.transparent = data < 1.0;
+          node.material.needsUpdate = true;
+        }
+      });
+    }
+  });
+
+
+
+
+//   scrolling
+
+
